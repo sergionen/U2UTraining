566f5aad117913b23286177ed7a7995a3dce1c54
Viktorwilczek <viktor-1996@live.com>
2022-11-10 10:26:25 +0100

    still not working

---
+++ WebShop.Infra/Configuration/OrderConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebShop.Core.Entities;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace WebShop.Infra.Configuration
+{
+    internal class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> order)
+        {
+            order.HasKey(o => o.Id);
+
+            order.HasMany(o => o.Products)
+                .WithOne();
+
+            order.Property(o => o.Total).HasColumnType("decimal(8,3)");
+
+        }
+        
+    }
+}
--- WebShop.Infra/Migrations/20221110092129_Order_Migration2.Designer.cs
+++ WebShop.Infra/Migrations/20221110092129_Order_Migration2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebShop.Infra.Repositories;
 
@@ -11,9 +12,10 @@ using WebShop.Infra.Repositories;
 namespace WebShop.Infra.Migrations
 {
     [DbContext(typeof(WebShopDbContext))]
-    partial class WebShopDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221110092129_Order_Migration2")]
+    partial class Order_Migration2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
