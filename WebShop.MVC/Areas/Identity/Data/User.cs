aa0a2f8322a683598a5826c7e01733d5d1b2d433
dieudo25 <twahirwa25@gmail.com>
2022-11-10 16:19:48 +0100

    Add logout

--- WebShop.MVC/Areas/Identity/Data/User.cs
+++ WebShop.MVC/Areas/Identity/Data/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
@@ -7,8 +8,8 @@ using Microsoft.AspNetCore.Identity;
 namespace WebShop.MVC.Areas.Identity.Data;
 
 // Add profile data for application users by adding properties to the User class
+[Table("AspNetUsers")]
 public class User : IdentityUser
 {
-    public string? Phone { get; set; }
 }
 
