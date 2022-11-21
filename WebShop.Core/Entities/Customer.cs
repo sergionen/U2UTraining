acc9e12fbc54099ed57aeb4a5d35c591951ae4c3
twahirwa25 <twahirwa25@hotmail.com>
2022-11-21 16:19:52 +0100

    Add import to template

---
+++ WebShop.Core/Entities/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebShop.MVC.Areas.Identity.Data;
+
+
+namespace WebShop.Core.Entities;
+
+public class Customer : User
+{
+}
